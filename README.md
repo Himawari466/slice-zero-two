9d5734359f4dc08275072a37a9638e1e32834cb8
Hmwr466 <selene.banderas.466@gmail.com>
2021-02-01 19:31:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Zero Two Design  
+===============  
+Licensed under [MIT License](https://mit-license.org/).
+  
+  
+### Tentang   
+Design dibuat oleh [Andre RIo](https://github.com/wibucode02).  
+Hasil slicing ini menggunakan CSS framwework [tailwind CSS](https://tailwindcss.com).  
+  
+Link design: [figma](https://www.figma.com/file/prWfLvlEpoWwM2PLuHMSpu/Landing-page---Zero-Two-Hero?node-id=4%3A2j)
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+        'dist/**/*.html',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    boxShadow: {
+        kado: '0 4px 2px 0 rgba(0, 0, 0, 0.2), 0 4px 4px 2px rgba(0, 0, 0, 0.05)',
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        none: 'none',
+    },
+    fontFamily: {
+        'body': ['Roboto'],
+    },
+    borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'lg30': '30px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
